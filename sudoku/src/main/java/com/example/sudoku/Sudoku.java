faada30f1f65c827fc4d9372e9d7b7b36b2526de
tiagoczz <tiagoeliassilva2005@gmail.com>
2024-12-24 13:35:17 -0300

    removendo os zeros do sudoku

--- sudoku/src/main/java/com/example/sudoku/Sudoku.java
+++ sudoku/src/main/java/com/example/sudoku/Sudoku.java
@@ -1,6 +1,19 @@
 package com.example.sudoku;
 
 public class Sudoku {
+    public static int randomN() {
+        int n;
+        while(true) {
+            n = (int)(Math.random() * 10);
+
+            if(n != 0) {
+                break;
+            }
+        }
+
+        return n;
+    }
+
     public static void main(String[] args) {
         int[][] sudoku = new int[9][9];
 
@@ -11,7 +24,8 @@ public class Sudoku {
                 if(j == 0) {
                    System.out.print("| ");
                 }
-                sudoku[i][j] = (int)(Math.random() * 10);
+
+                sudoku[i][j] = randomN();
                 System.out.print(sudoku[i][j] + " | ");
             }
             System.out.println();
